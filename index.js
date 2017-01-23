c1789c7ba8dbf4ffca280b6aa2b9ed2c7a575489
Zaheen Ahmed <zia1840@gmail.com>
2017-01-23 14:09:04 -0500

    Debug index.js
    removed route '/' from index.js, this will be handled by routes/index.js

--- index.js
+++ index.js
@@ -47,10 +47,6 @@ app.listen(app.get('port'), function(){
   console.log('Node app is running on port', app.get('port'));
 });
 
-app.get('/', function(req,res){
-  res.render('login')
-});
-
 app.get('*', function(req,res){
   res.render('page404', {req});
 });

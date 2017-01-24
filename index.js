77519226a2b412ebe91a2a82f440e9e685bcc812
Zaheen Ahmed <zia1840@gmail.com>
2017-01-24 13:26:40 -0500

    Adding passport authentication
    Attempt at adding authentication with passport.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var pg = require('pg');
 var bodyParser = require('body-parser');
+var session = require('express-session')
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var User = {
+  findOne: function(user, verify){
+
+  }
+};
 
 var app = express();
 app.set('port', (process.env.PORT || 5000));
@@ -8,13 +16,77 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser());
+app.use(session({
+  secret: 'no clue',
+  resave: false,
+  saveUninitialized: true}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 //connection string for connecting to postgres database
 var connectionString = process.env.DATABASE_URL;
 
+//authentication strategy
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    if (username === "zaheen" && password === "1234"){
+      return done(null, user = {
+        username: "zaheen",
+        fname: "Zaheen",
+        lname: "Ahmed"
+      });
+    }
+    return done(null, false);
+    // //connect to database
+    // pg.connect(connectionString, function(err,client,pgdone){
+    //   if(err){
+    //     return done(err);
+    //   }
+    //   //find matching user and password
+    //   client.query("select * from users where username = " + username + " and password = " + password + ";", function(err,result){
+    //     console.log(result);
+    //   }); //end client.query
+    // }); //end pg.connect
+  } //end strategy function
+)); //end passport.use(new LocalStrategy())
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
 app.listen(app.get('port'), function(){
   console.log('Node app is running on port', app.get('port'));
 });
+
+app.get('/', function(req,res){
+  pg.connect(connectionString, function(err,client,done){
+    if (err){
+      return console.log(err);
+    }
+    client.query("select * from users;", function(err,result){
+      if(err){
+        return console.log(err);
+      }
+      console.log(result);
+      done();
+      pg.end();
+    })
+  });
+  if(req.user){
+    res.render('user', {user: req.user});
+  }
+  else{
+    res.render('login');
+  }
+});
+
+app.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login'}));
 
 app.get('*', function(req,res){
   res.render('page404', {req});

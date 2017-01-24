7fe2c554a14af92f83c8b114bb777f0a242a187d
Zaheen Ahmed <zia1840@gmail.com>
2017-01-24 14:13:37 -0500

    debugging

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ passport.use(new LocalStrategy(
         return done(err);
       }
       //find matching user and password
-      client.query("select * from users;", function(err,result){
+      client.query("select * from users where username = '" + username + "' and password = '" + password + "';", function(err,result){
         if(err){
           console.log("error querying database");
           console.log(err);
